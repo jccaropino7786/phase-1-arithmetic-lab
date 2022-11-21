5cdfd17a149e47bdcbf42cbaf7df35372e16fe7a
john Caropino <jccaropino7786@live.com>
2022-11-21 10:55:22 -0800

    commit tree da7d2893c15f7d9a4e0a44a031025f1755897a27

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+console.log(random)
+
+let num3 =14;
+let num4 =5;
+let mod = num3 % num4;
+console.log(mod);
+
+
+let max=20
+for (let i = 0; i < max; i++){
+    console.count();
+}
